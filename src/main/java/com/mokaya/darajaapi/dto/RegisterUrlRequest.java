99af635ca5927acf03549bdcfade3aa2a291769b
brian-mokaya <brianmokaya507@gmail.com>
2025-07-20 00:39:04 +0300

    Annotate DTOs with `@JsonIgnoreProperties` to handle unknown JSON fields.

--- src/main/java/com/mokaya/darajaapi/dto/RegisterUrlRequest.java
+++ src/main/java/com/mokaya/darajaapi/dto/RegisterUrlRequest.java
@@ -1,9 +1,12 @@
 package com.mokaya.darajaapi.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 @Data
+
+//@JsonIgnoreProperties(ignoreUnknown = true)
 public class RegisterUrlRequest {
     @JsonProperty("shortCode")
     private String shortCode;
--- src/main/java/com/mokaya/darajaapi/dto/RegisterUrlResponse.java
+++ src/main/java/com/mokaya/darajaapi/dto/RegisterUrlResponse.java
@@ -1,9 +1,11 @@
 package com.mokaya.darajaapi.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 @Data
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class RegisterUrlResponse {
     @JsonProperty("conversationId")
     private String conversationId;
